d49a839f48e2542d89069c6aeba45e771838e200
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-19 19:55:44 +0200

    created views.py, server runs OK!

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Здесь будет главная страница нашей соц-сети')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Здесь будет cтраница сообщества {slug}')
