5db8ef96af50efab3ff0ac44f8bd538673f07290
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-21 14:41:54 +0200

    finally lauched Dev server

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+# Импортируем загрузчик.
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    template = '/d/Dev/yatube_project/yatube/templates/posts/index.html'
-    return render(request, template)
+    # Загружаем шаблон;
+    # шаблоны обычно хранят в отдельной директории.
+    template = loader.get_template('posts/index.html')
+    # Формируем шаблон
+    return HttpResponse(template.render({}, request)) 
 
 def group_posts(request, slug):
     return HttpResponse(f'Здесь будет cтраница сообщества {slug}')
