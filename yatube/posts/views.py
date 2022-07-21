63355cbc99c3a67cde64d104166db485b5b1445b
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-21 23:25:33 +0200

    added context dict for index.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template) 
+    title = 'Проект Yatube'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'title' : title,
+        'text': text,
+    }
+    return render(request, template, context) 
 
 def group_posts(request):
     template = 'posts/group_list.html'
