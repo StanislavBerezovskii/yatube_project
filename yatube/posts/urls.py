107bc71a3b6d0ab7b7c0c521a92ce950e3f118e0
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-22 21:38:52 +0200

    done with links, namespace, and names

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
-    path('group-list', views.group_posts),
-    path('base-template', views.show_base_template),
+    path('', views.index, name='index'),
+    path('group-list/', views.group_posts, name='group-list'),
+    path('base-template', views.show_base_template, name='base-template'),
 ]
