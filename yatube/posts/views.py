3f72f0d9c083348508e2cfbc28ffb4d834a31626
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-24 18:48:17 +0200

    title page ready!

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    title = 'Новости'
+    text = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
-    # В словаре context отправляем информацию в шаблон
     context = {
+        'title': title,
+        'text': text,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context) 
