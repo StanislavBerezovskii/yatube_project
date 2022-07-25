80947c10358d4c3fd500b54261b7fe9a2bdbea8c
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2022-07-25 17:01:04 +0200

    beta version ready

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,6 +7,6 @@ app_name = 'posts'
 urlpatterns = [
     # Главная страница
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('base_template/', views.show_base_template, name='show_base_template'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 def index(request):
-    title = 'Новости'
+    title = 'Последние обновления на сайте'
     text = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
@@ -16,8 +16,8 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    title = f'Последние записи сообщества {group.title}'
-    text = f'Последние записи сообщества {group.title}'
+    title = f'Записи сообщества {group.title}'
+    text = f'Записи сообщества {group.title}'
     context = {
         'group': group,
         'posts': posts,
